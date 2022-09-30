c06f25ab585aa69b2046a2674b6b9b8e6a9cd9fa
Aravind Kannan Rathinasabapathi <aravindkannan2001@gmail.com>
2022-09-30 19:47:40 +0530

    :construction: submit level-04 add required tests

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,9 +1,22 @@
 /* eslint-disable no-undef */
 const todoList = require("../index");
 
-const { all, markAsComplete, add } = todoList();
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 
 describe("Todolist Test Suite", () => {
+  const formattedDate = (d) => {
+    return d.toISOString().split("T")[0];
+  };
+
+  let dateToday = new Date();
+  const today = formattedDate(dateToday);
+  const yesterday = formattedDate(
+    new Date(new Date().setDate(dateToday.getDate() - 1))
+  );
+  const tomorrow = formattedDate(
+    new Date(new Date().setDate(dateToday.getDate() + 1))
+  );
+
   beforeAll(() => {
     add({
       title: "Test todo",
@@ -17,7 +30,7 @@ describe("Todolist Test Suite", () => {
     add({
       title: "Test todo",
       completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA"),
+      dueDate: today,
     });
     expect(all.length).toBe(todoItemsCount + 1);
   });
@@ -26,5 +39,35 @@ describe("Todolist Test Suite", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+
+  test("Should check retrival of overdue items", () => {
+    let overdueCount = overdue().length;
+    add({
+      title: "Overdue item",
+      completed: false,
+      dueDate: yesterday,
+    });
+    expect(overdue().length).toBe(overdueCount + 1);
+  });
+
+  test("Should check retrival of due today items", () => {
+    let todayItemsCount = dueToday().length;
+    add({
+      title: "Due today item",
+      completed: false,
+      dueDate: today,
+    });
+    expect(dueToday().length).toBe(todayItemsCount + 1);
+  });
+
+  test("Should check retrival of due later items", () => {
+    let dueLaterItemsCount = dueLater().length;
+    add({
+      title: "Due later item",
+      completed: false,
+      dueDate: tomorrow,
+    });
+    expect(dueLater().length).toBe(dueLaterItemsCount + 1);
   });
 });
