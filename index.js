228366489f1e38fe6281256800976ee8599d1d9b
Aravind Kannan Rathinasabapathi <aravindkannan2001@gmail.com>
2022-11-10 19:10:03 +0530

    :rocket: Debug error in prod

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-undef */
 const app = require("./app");
-app.listen(3000, () => {
-  console.log("Server started at: http://127.0.0.1:3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(
+    `Server started at: http://127.0.0.1:${process.env.PORT || 3000}`
+  );
 });
